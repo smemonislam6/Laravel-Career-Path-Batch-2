c32077e50d5dd4ef2ebcf5acd882e22254fc8b3b
SM Emon Islam <greensmdreem99@gmail.com>
2024-07-02 01:41:05 +0600

    composer update

--- Module-02 CLI/assignment/income-expense-app/vendor/composer/installed.php
+++ Module-02 CLI/assignment/income-expense-app/vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'smemo/income-expense-app',
+        'name' => 'smemo/income-expense-app-with-cli',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '672b2460224dd59f48b28a293aed276d6f6a04d8',
+        'reference' => '9dbb3d3310a3709c6dbcc3dedeccc98b42b57aad',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'smemo/income-expense-app' => array(
+        'smemo/income-expense-app-with-cli' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '672b2460224dd59f48b28a293aed276d6f6a04d8',
+            'reference' => '9dbb3d3310a3709c6dbcc3dedeccc98b42b57aad',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
