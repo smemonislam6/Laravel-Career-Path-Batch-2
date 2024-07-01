9dbb3d3310a3709c6dbcc3dedeccc98b42b57aad
SM Emon Islam <greensmdreem99@gmail.com>
2024-07-02 01:38:17 +0600

    Added Composer with autoloading.

---
+++ Module-02 CLI/assignment/income-expense-app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'smemo/income-expense-app',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '672b2460224dd59f48b28a293aed276d6f6a04d8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'smemo/income-expense-app' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '672b2460224dd59f48b28a293aed276d6f6a04d8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
